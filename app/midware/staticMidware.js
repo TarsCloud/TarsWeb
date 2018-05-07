e1c203ffcd9bbae7157cc37820e0d8b758868855
clauseliu <clauseliu@tencent.com>
2018-05-07 09:45:40 +0800

    clauseliu

--- app/midware/staticMidware.js
+++ app/midware/staticMidware.js
@@ -1,19 +1,18 @@
 var send = require('koa-send');
 var resolve = require('path').resolve;
-module.exports = (root, needNext, opt) =>{
+module.exports = (root, needNext, opt) => {
     opt = Object.assign({}, opt);
     opt.root = resolve(root);
-    return async function serve (ctx, next) {
+    return async(ctx, next) => {
         try {
-            await send(ctx, ctx.path, opt)
+            await send(ctx, ctx.path, opt);
         } catch (err) {
             if (err.status !== 404) {
-                throw err
+                throw err;
             }
         }
-        if(needNext){
-            await next()
+        if (needNext) {
+            await next();
         }
     };
 }
-
