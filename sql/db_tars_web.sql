65027ae7794c7235a9e4848073b643075aa293af
clauseliu <clauseliu@tencent.com>
2018-08-03 20:39:11 +0800

    clause

--- sql/db_tars_web.sql
+++ sql/db_tars_web.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.22, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db_tars_web
 -- ------------------------------------------------------
--- Server version	5.6.22
+-- Server version	5.5.28
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,57 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `t_server_user`
+-- Table structure for table `t_kafka_queue`
 --
 
-DROP TABLE IF EXISTS `t_server_user`;
+DROP TABLE IF EXISTS `t_kafka_queue`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `t_server_user` (
+CREATE TABLE `t_kafka_queue` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `application` varchar(128) NOT NULL,
-  `server_name` varchar(128) NOT NULL,
-  `read_role` text,
-  `write_role` text,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `application` (`application`,`server_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+  `topic` varchar(16) NOT NULL DEFAULT '',
+  `partition` int(4) NOT NULL DEFAULT '0',
+  `offset` int(11) NOT NULL DEFAULT '0',
+  `task_no` varchar(64) NOT NULL DEFAULT '' COMMENT '任务ID',
+  `status` varchar(16) NOT NULL DEFAULT 'waiting' COMMENT '任务状态',
+  `message` varchar(256) DEFAULT '',
+  PRIMARY KEY (`id`,`task_no`,`status`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `t_server_user`
+-- Table structure for table `t_patch_task`
 --
 
-LOCK TABLES `t_server_user` WRITE;
-/*!40000 ALTER TABLE `t_server_user` DISABLE KEYS */;
-INSERT INTO `t_server_user` VALUES (5,'Prajna','JesseGoServer','denisfan','denisfan'),(6,'Robin','TestServer','denisfan','denisfan'),(7,'Robin','TestServer1','denisfan','denisfan'),(8,'Robin','TestServer2','denisfan','denisfan'),(9,'Robin','TestServer3','denisfan','denisfan'),(10,'Robin','TestServer4','denisfan','denisfan'),(11,'tars','tarsconfig','denisfan','denisfan'),(12,'tars','tarslog','denisfan','denisfan'),(13,'tars','tarsnotify','denisfan','denisfan'),(14,'tars','tarspatch','denisfan','denisfan'),(15,'tars','tarsproperty','denisfan','denisfan'),(16,'tars','tarsqueryproperty','denisfan','denisfan'),(17,'tars','tarsquerystat','denisfan','denisfan'),(18,'tars','tarsstat','denisfan','denisfan'),(19,'Test','AServer','denisfan','denisfan'),(20,'Test','BServer','denisfan','denisfan'),(21,'Test','CServer','denisfan','denisfan'),(22,'Test','HelloServerI','denisfan','denisfan'),(23,'Test','HelloServerII','denisfan','denisfan'),(24,'Test','OkoNodejs','denisfan','denisfan'),(25,'Test','StressServer','denisfan','denisfan'),(26,'Test','TestPushServer','denisfan','denisfan'),(27,'Test','TestServer2','denisfan','denisfan'),(28,'Test','TestServer3','denisfan','denisfan'),(29,'TestApp','HelloJavaServer','denisfan','denisfan'),(30,'TestApp','HelloServer','denisfan','denisfan'),(31,'TestApp','ProductServer','denisfan','denisfan'),(32,'TestApp','TestJavaClient','denisfan','denisfan'),(33,'TestApp','TracingJavaOneServer','denisfan','denisfan'),(34,'TestApp','TracingJavaThreeServer','denisfan','denisfan'),(35,'TestApp','TracingJavaTwoServer','denisfan','denisfan'),(36,'TestJava','HelloServer','denisfan','denisfan'),(37,'TestJava','HelloServer2','denisfan','denisfan'),(38,'TestJava','JavaTestServer','denisfan','denisfan'),(39,'windweitest','windweiservera','denisfan','denisfan');
-/*!40000 ALTER TABLE `t_server_user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `t_user_info`
---
-
-DROP TABLE IF EXISTS `t_user_info`;
+DROP TABLE IF EXISTS `t_patch_task`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `t_user_info` (
+CREATE TABLE `t_patch_task` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(256) NOT NULL,
-  `password` varchar(256) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_name` (`user_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `server` varchar(50) DEFAULT NULL,
+  `tgz` text,
+  `task_id` varchar(64) DEFAULT NULL COMMENT '任务ID',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `t_user_info`
---
-
-LOCK TABLES `t_user_info` WRITE;
-/*!40000 ALTER TABLE `t_user_info` DISABLE KEYS */;
-/*!40000 ALTER TABLE `t_user_info` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -77,4 +59,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-19 11:02:19
+-- Dump completed on 2018-08-03 20:37:08
