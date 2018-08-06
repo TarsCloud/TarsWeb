be2c9360eac622b351d031fbed3c0cee44e5db14
clauseliu <clauseliu@tencent.com>
2018-08-06 11:34:40 +0800

    clauseliu

--- config/webConf.js
+++ config/webConf.js
@@ -1,5 +1,17 @@
 /**
- * Created by denisfan on 2018/4/8.
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
  */
 var path = require('path');
 
@@ -7,8 +19,8 @@ module.exports = {
     dbConf: {
         host: 'localhost',       // 数据库地址
         port: '3306',            // 数据库端口
-        user: 'root',            // 用户名
-        password: 'admin',       // 密码
+        user: 'tars',            // 用户名
+        password: 'tars2015',       // 密码
         charset: 'utf8_bin',     // 数据库编码
         pool: {
             max: 10,             // 连接池中最大连接数量
