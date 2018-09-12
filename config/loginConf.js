5d0579abc0ee0af4e7d12f693276153d8542cdbe
clauseliu <clauseliu@tencent.com>
2018-09-12 15:12:12 +0800

    clauseliu

--- config/loginConf.js
+++ config/loginConf.js
@@ -13,8 +13,7 @@
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
  * specific language governing permissions and limitations under the License.
  */
- 
-var path = require('path');
+
 
 /**
  * 登录配置
