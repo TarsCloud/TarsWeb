0e350c6c41a6f7acbe93b279fb330ba1199ea655
combineli <combineli@tencent.com>
2018-09-18 16:08:37 +0800

    添加磐石的配置

--- config/webConf.js
+++ config/webConf.js
@@ -38,7 +38,7 @@ module.exports = {
         calluser: 'tars',
         callsys: 'test_client',
         appkey: 'eff718a4894a42168eefee54ad134c51',
-        mappingUrl: 'http://localhost:7000/panshi/api'
+        mappingUrl: 'http://localhost:9089/panshi/api'
     },
     kafkaConf: {
         enable: false,
