07ef63e5978ac60d0460c59a4436faec2807c57e
combineli <combineli@tencent.com>
2019-04-15 10:09:47 +0800

    修复 扩容的 bug

--- client/src/dcache/interface.js
+++ client/src/dcache/interface.js
@@ -5,15 +5,14 @@ import Axios from '@/lib/axios.js'
  * @param servers
  * @param appName
  * @param moduleName
- * @param type
  * @param status
  * @param cache_version
  */
-export function expandModule ({servers, appName, moduleName, type = 'expand', status ='1', cache_version, srcGroupName = [], dstGroupName = []}) {
+export function expandModule ({servers, appName, moduleName, status ='1', cache_version, srcGroupName = [], dstGroupName = []}) {
   return Axios({
     method: 'post',
     url: '/cache/expandModule',
-    data: {appName, moduleName, servers, type, status, cache_version}
+    data: {appName, moduleName, servers, status, cache_version, srcGroupName, dstGroupName}
   })
 }
 
