51e4b038e5ccd195ccac0cc5cbc5fe6ea7850ecf
combineli <combineli@tencent.com>
2019-05-24 14:24:52 +0800

    添加 opt 请求日志

--- app/service/util/rpcClient/index.js
+++ app/service/util/rpcClient/index.js
@@ -19,39 +19,44 @@ const AdminRegProxy = require("./rpcProxy/AdminRegProxy");
 const ConfigFProxy = require("./rpcProxy/ConfigFProxy");
 const DCacheOptProxy = require("./rpcProxy/DCacheOptProxy");
 const path = require('path');
+const logger = require('./../../../logger');
 client.initialize(path.join(__dirname, '../../../../config/tars.conf'));
 const RPCClientPrx = (proxy, moduleName, interfaceName, servantName, setInfo) => {
     var module = proxy[moduleName];
     var rpcClient = client.stringToProxy(module[interfaceName+'Proxy'], servantName, setInfo);
-    for(var p in rpcClient){
+    for(let p in rpcClient){
         if(!rpcClient.hasOwnProperty(p) && p!='getTimeout' && p!='setTimeout'){
             ((p, fun) => {
-                rpcClient[p] = async function(...args){
-                    try{
-                        var _args = args;
-                        var rst = await (async ()=>{
-                            var result = await fun.apply(rpcClient, _args);
-                            var args = result.response.arguments;
-                            var rst = {__return: result.response.return};
-                            for(var p in args){
-                                if(typeof args[p] == 'object'){
-                                    rst[p] = args[p].toObject();
-                                }else{
-                                    rst[p] = args[p];
+                rpcClient[p] = (function(p) {
+                    let fnName = p;
+                    return async function(...args){
+                        try{
+                            var _args = args;
+                            var rst = await (async ()=>{
+                                var result = await fun.apply(rpcClient, _args);
+                                logger.info( 'method: ',fnName, ' request: ', _args, ' response: ', JSON.stringify(result.response));
+                                var args = result.response.arguments;
+                                var rst = {__return: result.response.return};
+                                for(var p in args){
+                                    if(typeof args[p] == 'object'){
+                                        rst[p] = args[p].toObject();
+                                    }else{
+                                        rst[p] = args[p];
+                                    }
                                 }
-                            }
+                                return rst;
+                            })();
                             return rst;
-                        })();
-                        return rst;
-                    }catch(e){
-                        console.error(e);
-                        if(e.response){
-                            throw new Error(e.response && e.response.error && e.response.error.message);
-                        }else{
-                            throw(e);
+                        }catch(e){
+                            console.error(e);
+                            if(e.response){
+                                throw new Error(e.response && e.response.error && e.response.error.message);
+                            }else{
+                                throw(e);
+                            }
                         }
-                    }
-                };
+                    };
+                })(p);
             })(p, rpcClient[p]);
 
         }
