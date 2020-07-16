1dfc4beb763942568ff55cf2644fa2486cfbc9aa
ruanshudong <ruanshudong@qq.com>
2020-07-16 16:48:43 +0800

    add db_tars_web_models

---
+++ app/dao/db/db_tars_web_models/t_business_application_relation.js
@@ -0,0 +1,61 @@
+/**
+ * Tencent is pleased to support the open source community by making Tars available.
+ *
+ * Copyright (C) 2016THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License"); you may not use this file except 
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed 
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR 
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations under the License.
+ */
+ 
+/* jshint indent: 1 */
+
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('t_business_application_relation', {
+		f_id: {
+			type: DataTypes.INTEGER(11),
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		f_business_name: {
+			type: DataTypes.STRING(64),
+			allowNull: false,
+			defaultValue: ''
+		},
+		f_application_name: {
+			type: DataTypes.STRING(64),
+			allowNull: false,
+			defaultValue: ''
+		},
+		f_create_person: {
+			type: DataTypes.STRING(64),
+			allowNull: false,
+			defaultValue: ''
+		},
+		f_create_time: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+		},
+		f_update_person: {
+			type: DataTypes.STRING(64),
+			allowNull: false,
+			defaultValue: ''
+		},
+		f_update_time: {
+			type: DataTypes.DATE,
+			allowNull: false,
+			defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+		},
+	}, {
+		tableName: 't_business_application_relation',
+		timestamps: false
+	});
+};
