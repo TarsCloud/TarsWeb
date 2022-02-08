87f7721a62106e66f8a90634f2c6ae708eabf146
ruanshudong <ruanshudong@qq.com>
2022-02-08 20:05:33 +0800

    update market version info

--- client/src/market/index.vue
+++ client/src/market/index.vue
@@ -7,15 +7,6 @@ export default {
   data() {
     return {};
   },
-  watch: {
-    // $route(to, from) {
-    //   let ticket = window.localStorage.ticket;
-    //   if (!ticket) {
-    //     this.onLogin(false);
-    //     return;
-    //   }
-    // },
-  },
   beforeRouteEnter(to, from, next) {
     next((vm) => {
       let ticket = window.localStorage.ticket;
@@ -25,48 +16,7 @@ export default {
       }
     });
   },
-  methods: {
-    // onLogin(isLogin) {
-    //   if (isLogin) {
-    //     this.$store.commit({
-    //       type: "marketUid",
-    //       uid: window.localStorage.uid,
-    //     });
-    //   }
-    //   if (!isLogin && !location.hash.startsWith("#/market/user")) {
-    //     this.$router.push("/market/user/login");
-    //   } else if (isLogin && location.hash == "#/market") {
-    //     this.$router.push("/market/list");
-    //   }
-    // },
-    // checkLogin() {
-    //   let ticket = window.localStorage.ticket;
-    //   if (!ticket) {
-    //     this.onLogin(false);
-    //     return;
-    //   }
-    //   this.$market
-    //     .call("cloud-user", "isLogin", {
-    //       ticket: ticket,
-    //     })
-    //     .then((data) => {
-    //       if (!data.uid) {
-    //         this.onLogin(false);
-    //       } else {
-    //         window.localStorage.uid = data.uid;
-    //         this.onLogin(true);
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       this.$message({
-    //         message: err,
-    //         type: "error",
-    //       });
-    //     });
-    // },
-  },
-  mounted() {
-    // this.checkLogin();
-  },
+  methods: {},
+  mounted() {},
 };
 </script>
