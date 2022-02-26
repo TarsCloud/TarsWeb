0699961dd8aede015d2f8001a108d6b7f036ba8a
ruanshudong <ruanshudong@qq.com>
2022-02-26 16:01:31 +0800

    fix sso page login bug

--- client/src/sso/adminPass/adminPass.js
+++ client/src/sso/adminPass/adminPass.js
@@ -4,16 +4,27 @@ import '../../plugins/ui';
 import '../../plugins/ajax';
 
 import adminPass from './adminPass.vue';
-import {i18n, loadLang} from '../../locale/i18n'
+import {
+  i18n,
+  loadLang
+} from '../../locale/i18n'
+import ElementUI from 'element-ui';
+
+import "@/assets/theme/element-to-let/index.css"
 
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
-loadLang.call(this).then(()=> {
+loadLang.call(this).then(() => {
+  Vue.use(ElementUI, {
+    i18n: (key, value) => i18n.t(key, value)
+  });
   new Vue({
     el: '#admin-pass-app',
     i18n,
-    components: {adminPass},
+    components: {
+      adminPass
+    },
     template: '<admin-pass/>',
   });
 });
