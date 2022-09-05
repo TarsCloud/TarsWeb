55e21819b9aaf6bf598a335232bf73ddd7fb705b
ruanshudong <ruanshudong@qq.com>
2022-09-05 14:34:11 +0800

    fix: preparePatch check file exists

--- app/init/tars.js
+++ app/init/tars.js
@@ -24,6 +24,7 @@ var fs = require("fs")
 var path = require("path")
 const md5Sum = require('md5-file').sync;
 const logger = require('../../logger');
+const { off } = require('process');
 
 const TarsInit = {};
 
@@ -31,7 +32,7 @@ TarsInit.deleteDirectory = (dir) => {
     try {
         if (fs.existsSync(dir) == true) {
             var files = fs.readdirSync(dir);
-            files.forEach(function(item){
+            files.forEach(function (item) {
                 var item_path = path.join(dir, item);
                 if (fs.statSync(item_path).isDirectory()) {
                     TarsInit.deleteDirectory(item_path);
@@ -42,15 +43,15 @@ TarsInit.deleteDirectory = (dir) => {
             });
             fs.rmdirSync(dir);
         }
-    }catch(e) {
+    } catch (e) {
         logger.error('deleteDirectory error:', e);
     }
 }
- 
-TarsInit.insert = async(patchTmp, patchPath, filePath, file)=> {
+
+TarsInit.insert = async (patchTmp, patchPath, filePath, file) => {
 
     let name = file.split('.');
-    if(name.length != 2 || name[1] != 'tgz' || name[0].indexOf('tars') != 0) {
+    if (name.length != 2 || name[1] != 'tgz' || name[0].indexOf('tars') != 0) {
         return;
     }
 
@@ -61,10 +62,10 @@ TarsInit.insert = async(patchTmp, patchPath, filePath, file)=> {
 
     let patch = await PatchDao.find({
         server: server,
-        version: hash 
+        version: hash
     });
 
-    if(!patch) {
+    if (!patch) {
 
         let dstTmp = patchTmp + '/' + name[0] + '.tar';
         logger.info("uncompress tgz -> tar:", src, dstTmp)
@@ -74,10 +75,10 @@ TarsInit.insert = async(patchTmp, patchPath, filePath, file)=> {
         await compressing.tar.uncompress(dstTmp, patchTmp);
 
         //移动可执行文件到上一层目录
-        let newPatchPath = patchPath +'/' + name[0];
+        let newPatchPath = patchPath + '/' + name[0];
 
         logger.info("mkdir :", newPatchPath);
-        fs.mkdirSync(newPatchPath, {recursive: true});
+        fs.mkdirSync(newPatchPath, { recursive: true });
 
         logger.info("rename :", patchTmp + '/' + name[0] + '/bin/' + name[0], ' -> ', newPatchPath + '/' + name[0]);
         fs.renameSync(patchTmp + '/' + name[0] + '/bin/' + name[0], newPatchPath + '/' + name[0]);
@@ -88,7 +89,7 @@ TarsInit.insert = async(patchTmp, patchPath, filePath, file)=> {
         await compressing.gzip.compressFile(newPatchPath + '.tar', newPatchPath + '.tgz');
 
         logger.info('delete dir:', newPatchPath);
-        TarsInit.deleteDirectory(newPatchPath); 
+        TarsInit.deleteDirectory(newPatchPath);
 
         logger.info('delete file:', newPatchPath + '.tar');
         fs.unlinkSync(newPatchPath + '.tar');
@@ -96,7 +97,7 @@ TarsInit.insert = async(patchTmp, patchPath, filePath, file)=> {
         //copy 到tars.upload目录下
         let dst = webConf.pkgUploadPath.path + '/tars/' + name[0] + '/';
 
-        fs.mkdirSync(dst, {recursive: true});
+        fs.mkdirSync(dst, { recursive: true });
 
         let dstFile = dst + '/' + name[0] + ".tgz";
         fs.copyFileSync(newPatchPath + '.tgz', dstFile);
@@ -123,7 +124,7 @@ TarsInit.insert = async(patchTmp, patchPath, filePath, file)=> {
 
 };
 
-TarsInit.preparePatch = async() => {
+TarsInit.preparePatch = async () => {
 
     var dir = path.join(__dirname, "../../files");
 
@@ -134,11 +135,15 @@ TarsInit.preparePatch = async() => {
 
             let patchTmp = dir + '/patchTmp';
             let patchPath = dir + '/patch';
-    
-            fs.mkdirSync(patchTmp, { recursive: true });
-            fs.mkdirSync(patchPath, { recursive: true });
 
-            logger.info("mkdir :", patchTmp, patchPath);
+            if (!fs.existsSync(patchTmp)) {
+                fs.mkdirSync(patchTmp, { recursive: true });
+            }
+
+            if (!fs.existsSync(patchPath)) {
+                fs.mkdirSync(patchPath, { recursive: true });
+                logger.info("mkdir :", patchTmp, patchPath);
+            }
 
             files.map(async (file) => {
                 let filePath = path.join(dir, file);
